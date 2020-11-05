1a50d2e39838e3f50fda4ea049a958ea307b976c
Nidheesh <52731229+nidheeshs@users.noreply.github.com>
2020-11-05 12:45:35 +0530

    Add files via upload

---
+++ Novel corona.py
@@ -0,0 +1,66 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar 28 11:20:10 2020
+
+@author: Nidhi
+"""
+
+#import libraries
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+from scipy import stats
+from scipy.stats import norm,skew
+
+
+#import and display dataset
+confirmed_df=pd.read_csv("time_series_covid_19_confirmed.csv")
+deaths_df=pd.read_csv("time_series_covid_19_deaths.csv")
+recovered_df=pd.read_csv("time_series_covid_19_recovered.csv")
+df.info()
+confirmed_df.head().transpose()
+deaths_df.head().transpose()
+recovered_df.head().transpose()
+
+
+df.isnull().sum()
+confirmed_df['Country/Region'].value_counts()
+
+
+dates = list(confirmed_df.columns[4:])
+dates = list(pd.to_datetime(dates))
+dates_india = dates[8:]
+dates_india[:-1]
+
+df=confirmed_df.groupby('Country/Region').sum()
+
+df1 = confirmed_df.groupby('Country/Region').sum().reset_index()
+df2 = deaths_df.groupby('Country/Region').sum().reset_index()
+df3 = recovered_df.groupby('Country/Region').sum().reset_index()
+
+k = df1[df1['Country/Region']=='India'].loc[:,'1/30/20':]
+india_confirmed = k.values.tolist()[0] 
+
+k = df2[df2['Country/Region']=='India'].loc[:,'1/30/20':]
+india_deaths = k.values.tolist()[0] 
+
+k = df3[df3['Country/Region']=='India'].loc[:,'1/30/20':]
+india_recovered = k.values.tolist()[0] 
+
+plt.figure(figsize= (15,10))
+plt.xticks(rotation = 90 ,fontsize = 11)
+plt.yticks(fontsize = 10)
+plt.xlabel("Dates",fontsize = 20)
+plt.ylabel('Total cases',fontsize = 20)
+plt.title("Total Confirmed, Active, Death in India" , fontsize = 20)
+
+ax1 = plt.plot_date(y= india_confirmed,x= dates_india,label = 'Confirmed',linestyle ='-',color = 'b')
+ax2 = plt.plot_date(y= india_recovered,x= dates_india,label = 'Recovered',linestyle ='-',color = 'g')
+ax3 = plt.plot_date(y= india_deaths,x= dates_india,label = 'Death',linestyle ='-',color = 'r')
+plt.legend();
+plt.show()
+
+
+
